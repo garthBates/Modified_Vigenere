d2c88c29782433be6a7c999a039478e29c6769d6
Garth <garthbates@protonmail.com>
2023-11-11 13:58:28 -0800

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+///////////////////////////////
+// Programmer: Garth Bates
+// Project: Modified Vigenere
+// File: main.cpp
+// Date Started: 11/11/2023
+// Date Completed:
+///////////////////////////////
+
+int main(void){
+    return 0;
+}
