e29b95aaebe21e8212658a28706c54fb6f0e16e0
Garth <garthbates@protonmail.com>
2023-12-09 13:17:51 -0800

    Added menu functionality to main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,29 @@
 int main(void){
     Cypher cyp;
     //call runEncryption to start debugging
-    cout << cyp.runEncryption();
-    cout << cyp.runDecryption();
+    //cout << cyp.runEncryption();
+    //cout << cyp.runDecryption();
+
+    int option = -1;
+
+    while(option != 0){
+        cout << "Press 1 to encrypt text" << endl;
+        cout << "Press 2 to decrypt text" << endl;
+        cout << "Press 0 to quit" << endl;
+
+        cin >> option;
+
+        if (option == 1){
+            cyp.runEncryption();
+        } else if (option == 2){
+            cyp.runDecryption();
+        } else if (option == 0) {
+            cout << "Ending operations" << endl;
+        } else {
+            cout << "Invalid input" << endl;
+        }
+
+
+    }
+
 }
